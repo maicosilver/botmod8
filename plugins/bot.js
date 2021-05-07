dc6255467f07346dcaaf27c8660082bc4338c920
maicosilver <77900033+maicosilver@users.noreply.github.com>
2021-05-07 17:28:15 -0300

    Update bot.js

--- plugins/bot.js
+++ plugins/bot.js
@@ -1,5 +1,9 @@
+let gtts = require('node-gtts')
+let fs = require('fs')
+let path = require('path')
+let { spawn } = require('child_process')
 let handler  = async (m, { conn }) => {
-       conn.sendFile(m.chat, 'media/bot.mp3', '', 'xd', m)
+	conn.sendFile(m.chat, 'media/bot.mp3', 'tts.opus', null, m, true)
 }
 handler.command = /^(bot)$/i
 handler.fail = null

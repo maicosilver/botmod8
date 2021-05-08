f079cd381ef07900497c1a7391f4e935de46bb9a
maicosilver <77900033+maicosilver@users.noreply.github.com>
2021-05-08 13:04:09 -0300

    Update twitter.js

--- plugins/twitter.js
+++ plugins/twitter.js
@@ -1,14 +1,7 @@
-let handler = async (m, { conn, args }) => {
-  throw // Fitur belum jadi :)
-  if (!args[0]) throw 'Uhm...url nya mana?'
-  global.API('xteam', '/dl/twitter', {
-    url: args[0]
-  }, 'APIKEY')
-  conn.sendFile(m.chat, undefined, '', '', m)
+let handler  = async (m, { conn }) => {
+       conn.sendFile(m.chat, 'media/Buenos días.mp3', '', 'xd', m)
 }
-handler.help = [].map(v => v + ' <url>')
-handler.tags = ['downloader']
-
-handler.command = /^$/i
-
+handler.command = /^(bd)$/i
+handler.fail = null
+handler.exp = 100
 module.exports = handler
